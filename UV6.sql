85f4a2ef6b6daf6d3da4d31952f4e42c9e7125f4
Har-Mar <75963605+Har-Mar@users.noreply.github.com>
2021-01-04 15:55:31 -0600

    Delete UV6.sql

--- UV6.sql
+++
@@ -1,7 +0,0 @@
-PROMPT USERVIEW 6
-
-SET LINESIZE 250
-SET PAGESIZE 250
-SELECT o.order_ID, o.orderquantity, oi.order_date, oi.shipped_date, p.model, p.make_year, p.scale
-FROM orders o, orderitem oi, product p
-WHERE o.order_ID = oi.order_ID AND p.product_ID = o.product_ID;
