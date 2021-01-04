199a5ce603c46973e9eee004ae50f7565a1d2620
Har-Mar <75963605+Har-Mar@users.noreply.github.com>
2021-01-04 15:55:24 -0600

    Delete UV5.sql

--- UV5.sql
+++
@@ -1,7 +0,0 @@
-PROMPT USERVIEW 5
-
-SET LINESIZE 250
-SET PAGESIZE 250
-SELECT py.payment_ID, py.check_number, py.payment_date, o.orderquantity, o.Total_Price, o.Product_ID
-FROM payment py, orders o
-WHERE py.payment_ID = o.payment_ID;
